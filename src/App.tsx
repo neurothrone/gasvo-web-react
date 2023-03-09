8d671caaaa9a66a110e6d11df5ddc68e01b23fe1
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-03-09 16:18:54 +0100

    add initial form logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+const defaultFormData = {
+  nps: "",
+  length: "",
+  pressure: "",
+};
 
 function App() {
+  const [formData, setFormData] = useState(defaultFormData);
+  const {nps, length, pressure} = formData;
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(formData);
+    // setFormData(defaultFormData);
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>GasVo</h1>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="nps">NPS</label>
+        <br/>
+        <input type="text" id="nps" value={nps} onChange={onChange}/>
+        <br/>
+        <br/>
+
+        <label htmlFor="length">Length</label>
+        <br/>
+        <input type="text" id="length" value={length} onChange={onChange}/>
+        <br/>
+        <br/>
+
+        <label htmlFor="pressure">Pressure</label>
+        <br/>
+        <input type="text" id="pressure" value={pressure} onChange={onChange}/>
+        <br/>
+        <br/>
+
+        <button type="submit">Upload post</button>
+      </form>
+    </>
   );
 }
 
