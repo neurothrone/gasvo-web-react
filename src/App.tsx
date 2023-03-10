8ff3967d3164a47df59d274f82d3a1b73c375405
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-03-10 13:31:18 +0100

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 import CalculateForm from "./components/CalculateForm";
 import GasvoCore from "./lib/GasvoCore";
+import Footer from "./components/Footer";
 
 function App() {
   const [gasVolume, setGasVolume] = useState<number>(0);
@@ -18,7 +19,10 @@ function App() {
       <h1>GasVo</h1>
       <h2>Gas Pipe Volume Calculator</h2>
       <CalculateForm onCalculate={handleCalculate} onReset={() => setGasVolume(0)}/>
-      <p>Gas Volume: {getGasVolume()}</p>
+      <h3>Gas Volume (m<sup>3</sup>)</h3>
+      <p>{getGasVolume()}</p>
+
+      <Footer/>
     </>
   );
 }
