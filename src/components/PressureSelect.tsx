d3513f1179827f102974e88f89d63ec41b18efa2
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-03-12 15:24:56 +0100

    format code with prettier

--- src/components/PressureSelect.tsx
+++ src/components/PressureSelect.tsx
@@ -1,23 +1,24 @@
-import {getPressureLabels, PressureType} from "../lib/PressureType";
+import { getPressureLabels, PressureType } from "../lib/PressureType";
 
 type PressureSelectProps = {
   pressureType: PressureType;
   onPressureClick: (pressureType: PressureType) => void;
   className?: string;
-}
+};
 
-function PressureSelect({pressureType, onPressureClick, className}: PressureSelectProps) {
-
-  const getClass = (p: PressureType) => p === pressureType ? "btn-primary" : "btn-outline-primary";
+function PressureSelect({ pressureType, onPressureClick, className }: PressureSelectProps) {
+  const getClass = (p: PressureType) =>
+    p === pressureType ? "btn-primary" : "btn-outline-primary";
 
   return (
     <div>
-      {getPressureLabels().map(pressureValue => (
+      {getPressureLabels().map((pressureValue) => (
         <button
           className={className + " w-25 btn " + getClass(pressureValue as PressureType)}
           key={pressureValue}
           onClick={() => onPressureClick(pressureValue as PressureType)}
-        >{pressureValue}
+        >
+          {pressureValue}
         </button>
       ))}
     </div>
